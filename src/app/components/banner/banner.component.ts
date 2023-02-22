3acd82b86506dd54cdf73478be708097fde4d6ec
Juan Martin Castañar Petarcich <juanpetarcich@gmail.com>
2023-02-22 15:30:24 -0300

    Social media menu

--- src/app/components/banner/banner.component.ts
+++ src/app/components/banner/banner.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { persona } from 'src/app/model/persona.model';
+import { PersonaService } from 'src/app/service/persona.service';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
   styleUrls: ['./banner.component.css']
 })
-export class BannerComponent {
+export class BannerComponent implements OnInit {
+  persona: persona = new persona("","","");
 
-}
+  constructor(public personaService: PersonaService){ }
+
+  ngOnInit(): void {
+    this.personaService.getPersona().subscribe((data: persona) => {this.persona = data})   
+  }
+
+}  
